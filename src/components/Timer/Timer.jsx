97c4abd9d888fd678257610c2f9c95aaa8d195bd
Ghanshyam <gk.paircode@gmail.com>
2023-07-08 11:10:52 +0530

    useEfect with empty dependency array

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -4,13 +4,23 @@ export const Timer = () => {
     const [time, setTime] = useState(new Date().getTime());
     useEffect(() => {
         setTimeout(() => {
-            setTime(oldTime => oldTime + 1000);
-        }, 1000);
-    });
-
+            setTime(oldTime => {
+                console.log( new Date(oldTime).toString() , ' --> ',  new Date(oldTime + 1000).toString() );
+                return oldTime + 1000;
+            });
+        }, 3000);
+    }, []);
+    
     return(
         <div>
             Current Time : {time} - { new Date(time).toString() }
         </div>
     )
 }
+    /*  
+        here dependency array is empty,
+        so this effect will be executed when componnet will mount
+        In react 18, effect gets executed twice, but in older react only once.
+        In order to execute effect only once (on mount), you can unwrap the App component from StrictMode component.
+        But it is not recommended.
+    */
