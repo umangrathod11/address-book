a75c73d9831458714a3676bb9a2ad020f15c42e2
gani-paircode <gk.paircode@gmail.com>
2023-07-10 19:02:59 +0530

    testing usecallback with and without dep array item

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import "./styles.css";
-import { UseRefDemo1, UseRefDemo2 } from './components/UseRefDemo';
 
 export default function App() {
+  const [names, setNames] = useState(['raj']);
+  const nameRef = useRef();
+  const [randomNum, setRandomNum] = useState(0);
+  const addNames = useCallback(() => {
+    const newNames = [...names, nameRef.current.value];
+    setNames(newNames);
+    nameRef.current.value = '';
+    nameRef.current.focus();
+  }, [names]);
+  
+
+  const addNames2 = useCallback(() => {
+    const newNames = [...names, nameRef.current.value];
+    setNames(newNames);
+    nameRef.current.value = '';
+    nameRef.current.focus();
+  }, []);
+  
   return (
     <div className="App">
-      <UseRefDemo1 />
+      <input id="name1" ref={nameRef} />
+      <button onClick={addNames}>Add Btn 1</button>
+      &nbsp;&nbsp;
+      <button onClick={addNames2}>Add Btn 2</button>
+
       <br />
-      <br />
-      <br />
-      {/* <UseRefDemo2 /> */}
+      <h3>{names.join(", ")}</h3>
+      <h3 onClick={(e) => setRandomNum(Math.random())}>Random number : {randomNum}</h3>
     </div>
   );
 }
