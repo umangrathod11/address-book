32c7f9bb823a31dd063fe5c76f1380f74651e5aa
Ghanshyam <gk.paircode@gmail.com>
2023-07-12 23:22:02 +0530

    cleaned up App.js

--- src/App.js
+++ src/App.js
@@ -1,51 +1,11 @@
 import React, { useCallback, useId, useRef, useState } from 'react';
 import "./styles.css";
 
-export default function App() {
-  const [randomNum, setRandomNum] = useState(0);
-  
+export default function App() {  
   return (
     <div className="App">
-      <NamesForm key={randomNum} randomNum={randomNum} generateRandomNum={() => setRandomNum(Math.random())} />
+      
     </div>
   );
 }
 
-export const NamesForm = ({ randomNum, generateRandomNum }) => {
-
-  const [names, setNames] = useState(['raj']);
-  const nameRef = useRef();
-
-  const addNames = useCallback(() => {
-    const newNames = [...names, nameRef.current.value];
-    setNames(newNames);
-    nameRef.current.value = '';
-    nameRef.current.focus();
-  }, [names]);
-  
-  return (
-    <div>
-      <input id="name1" ref={nameRef} />
-      <button onClick={addNames}>Add Btn 1</button>
-
-      <br />
-      <br />
-      <h3 onClick={generateRandomNum}>Random number : {randomNum}. Click here to generate new one.</h3>
-      <br />
-      <br />
-      <NamesList names={names} />
-    </div>
-  )
-}
-
-const NamesList = ({ names }) => {
-  const uniqId = useId();
-  return (
-    <div>
-      <h3>Names : {names.join(", ")}</h3>
-      <br />
-      <br />
-      <h3>Uniq id : {uniqId}</h3>
-    </div>
-  )
-}
