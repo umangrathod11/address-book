7c3530e577f6bd9fa47eec6505608ddce3ab11ad
gani-paircode <gk.paircode@gmail.com>
2023-07-18 19:15:29 +0530

    Handled 404 with in a route

--- src/pages/Community/TabComponents/MembersLanding.jsx
+++ src/pages/Community/TabComponents/MembersLanding.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { MemberDetails } from './MemberDetails';
 import { MemberForm } from './MemberForm';
 import { MembersList } from './MembersList';
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes, useNavigate } from 'react-router-dom';
+import { TAB_IDS } from '../../../constants/general';
 
 export const MembersLanding = () => {
     return (
@@ -11,7 +12,29 @@ export const MembersLanding = () => {
                 <Route index element={<MembersList />} />
                 <Route path=":memberId" element={<MemberDetails />} />
                 <Route path="add" element={<MemberForm />} />
+                <Route path="*" element={<InvalidMembersPath />} />
             </Routes>
         </>
     )
 }
+
+// We have 2 implementation of InvalidMembersPath, both are valid
+
+const InvalidMembersPath = () => {
+    return (
+        <div>
+            <h2>Oops !!. At this time we don't have any content to show you for the given URL.</h2>
+            <h3>Please click <Link to={`/${TAB_IDS.MEMBERS}`}>here</Link> to go to members list page</h3>
+        </div>
+    )
+}
+
+// const InvalidMembersPath = () => {
+//     const navigate = useNavigate();
+//     return (
+//         <div>
+//             <h2>Oops !!. At this time we don't have any content to show you for the given URL.</h2>
+//             <h3>Please click <a href="" onClick={() => navigate(`/${TAB_IDS.MEMBERS}`)}>here</a> to go to members list page</h3>
+//         </div>
+//     )
+// }
