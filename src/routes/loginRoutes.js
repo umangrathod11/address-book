46ae7aeac5895cb3edce3e08ab9d914ce0860b81
Ghanshyam <gk.paircode@gmail.com>
2023-07-22 22:46:23 +0530

    wip

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -20,14 +20,15 @@ router.post('/v1', async (req, res) => {
   try {
     let users = await readDataFromFile(LOGIN_FILE_PATH);
     const user = users.find((u) => u.phoneNumber === phoneNumber);
-    if (user.isActive === false) {
-      res.status(400).send({ message: 'User is deactivated. Please contact admin.' });
-      return;
-    }
     if (!user) {
       res.status(400).send({ message: 'Invalid id or password' });
       return;
     }
+    if (user.isActive === false) {
+      res.status(400).send({ message: 'User is deactivated. Please contact admin.' });
+      return;
+    }
+ 
     const hashedPassword = getHashedPassword(password);
     if (user.hashedPassword !== hashedPassword) {
       res.status(400).send({ message: 'Invalid password' });
